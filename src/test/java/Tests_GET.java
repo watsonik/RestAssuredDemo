480f0521e1d6fe8fa27d6a557f258ad3371642d5
Sakun, Ihar <Ihar.Sakun@McKesson.com>
2020-08-03 22:53:44 +0300

    hasItems

--- src/test/java/Tests_GET.java
+++ src/test/java/Tests_GET.java
@@ -12,6 +12,7 @@ public class Tests_GET {
     public void test_1() {
         given().get("https://reqres.in/api/users?page=2").then().statusCode(200)
                 .body("data.id[1]",equalTo(8))
+                .body("data.first_name", hasItems("Lindsay"))
                 .log().all();
     }
 
