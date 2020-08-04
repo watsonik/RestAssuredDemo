a565ef294ba5b180d774e5d5fb53b94c80ac4082
Sakun, Ihar <watsonik@gmail.com>
2020-08-04 22:57:32 +0300

    hasItems

--- src/test/java/Tests_POST.java
+++ src/test/java/Tests_POST.java
@@ -1,7 +1,7 @@
+import com.google.gson.JsonObject;
 import org.testng.annotations.Test;
 
-import java.util.HashMap;
-import java.util.Map;
+import static io.restassured.RestAssured.*;
 
 /**
  * @author Ihar_Sakun
@@ -10,9 +10,23 @@ public class Tests_POST {
 
     @Test
     public void test_1_post() {
-        Map<String, Object> map = new HashMap<>();
-        map.put("name", "Ihar");
-        map.put("job", "epam");
-        System.out.println(map);
+//        Map<String, Object> map = new HashMap<>();
+//        map.put("name", "Ihar");
+//        map.put("job", "epam");
+//        System.out.println(map);
+        JsonObject request = new JsonObject();
+        request.addProperty("name", "Ihar");
+        request.addProperty("job", "epam");
+        System.out.println(request);
+        given().body(request).post("https://reqres.in/api/users").then().statusCode(201).log().all();
+    }
+
+    @Test
+    public void test_1_put() {
+        JsonObject request = new JsonObject();
+        request.addProperty("name", "Ihar");
+        request.addProperty("job", "epam");
+        System.out.println(request);
+        given().body(request).when().patch("https://reqres.in/api/users/2").then().statusCode(200).log().all();
     }
 }
