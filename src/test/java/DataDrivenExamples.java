35a156c923b61f5ce04c8faab82545ad228574f7
Sakun, Ihar <watsonik@gmail.com>
2020-08-05 14:26:35 +0300

    hasItems

--- src/test/java/DataDrivenExamples.java
+++ src/test/java/DataDrivenExamples.java
@@ -12,14 +12,18 @@ public class DataDrivenExamples {
 
     @DataProvider(name = "DataForPost")
     public Object[][] dataForPost() {
-        Object[][] data = new Object[2][3];
-        data[0][0] = "Albert";
-        data[0][1] = "Einstein";
-        data[0][2] = 2;
-        data[1][0] = "Thomas";
-        data[1][1] = "Edison";
-        data[1][2] = 1;
-        return data;
+//        Object[][] data = new Object[2][3];
+//        data[0][0] = "Albert";
+//        data[0][1] = "Einstein";
+//        data[0][2] = 2;
+//        data[1][0] = "Thomas";
+//        data[1][1] = "Edison";
+//        data[1][2] = 1;
+//        return data;
+        return new Object[][]{
+            {"Graham","Bell",1},
+            {"Henry","Ford",2},
+        };
     }
 
     @Test(dataProvider = "DataForPost")
