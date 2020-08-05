4b9130b4e6dd1c0bc656c7f34122f11fbf7cf829
Sakun, Ihar <watsonik@gmail.com>
2020-08-05 15:49:15 +0300

    Parameterization

--- src/test/java/DataDrivenExamples.java
+++ src/test/java/DataDrivenExamples.java
@@ -1,6 +1,7 @@
 import com.google.gson.JsonObject;
 import io.restassured.http.ContentType;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
@@ -8,23 +9,7 @@ import static io.restassured.RestAssured.*;
 /**
  * @author Ihar_Sakun
  */
-public class DataDrivenExamples {
-
-    @DataProvider(name = "DataForPost")
-    public Object[][] dataForPost() {
-//        Object[][] data = new Object[2][3];
-//        data[0][0] = "Albert";
-//        data[0][1] = "Einstein";
-//        data[0][2] = 2;
-//        data[1][0] = "Thomas";
-//        data[1][1] = "Edison";
-//        data[1][2] = 1;
-//        return data;
-        return new Object[][]{
-            {"Graham","Bell",1},
-            {"Henry","Ford",2},
-        };
-    }
+public class DataDrivenExamples extends DataForTests {
 
     @Test(dataProvider = "DataForPost")
     public void test_post(String firstName, String lastName, int subjectId) {
@@ -41,6 +26,22 @@ public class DataDrivenExamples {
                 post("/users").
                 then().
                 statusCode(201).log().all();
+    }
+
+    @Test(dataProvider = "DeleteData")
+    public void test_delete(int userId) {
+        baseURI = "http://localhost:3000";
+        when().
+                delete("/users/" + userId).then().statusCode(200);
+    }
+
+    @Parameters({"userId"})
+    @Test
+    public void test_delete2(int userId) {
+        System.out.println("Value for userId is:"+userId);
+        baseURI = "http://localhost:3000";
+        when().
+                delete("/users/" + userId).then().statusCode(200);
     }
 
 }
